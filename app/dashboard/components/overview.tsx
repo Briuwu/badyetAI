0182d6586a54a66274b359a96835af88ff7b08e1
Briuwu <millontebrian@gmail.com>
2025-04-30 15:25:44 +0800

    feat: refactor Overview component to dynamically display income, expense, and savings totals

--- app/dashboard/components/overview.tsx
+++ app/dashboard/components/overview.tsx
@@ -3,22 +3,55 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { ArrowUp, EllipsisVertical } from "lucide-react";
+import { getGoals } from "@/lib/goals";
+import { getTransactions } from "@/lib/transactions";
+import { cn } from "@/lib/utils";
+import { EllipsisVertical } from "lucide-react";
 
-export const Overview = () => {
+export const Overview = async () => {
+  const transactions = await getTransactions();
+  const goals = await getGoals();
+
+  const totalSavings = goals.reduce((acc, goal) => acc + goal.saved_amount, 0);
+
+  const totalIncome = transactions
+    .filter((transaction) => transaction.transaction_type === "income")
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  const totalExpense = transactions
+    .filter((transaction) => transaction.transaction_type === "expense")
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  const overview = [
+    {
+      title: "Total Income",
+      amount: totalIncome,
+      color: "text-green-500",
+    },
+    {
+      title: "Total Expense",
+      amount: totalExpense,
+      color: "text-red-500",
+    },
+    {
+      title: "Total Savings",
+      amount: totalSavings,
+      color: "text-blue-500",
+    },
+  ];
+
   return (
     <div className="my-10 grid gap-5 lg:grid-cols-3">
-      {Array.from({ length: 3 }, (_, index) => (
-        <Card key={index}>
+      {overview.map((item) => (
+        <Card key={item.title}>
           <CardHeader className="flex items-center justify-between text-slate-800">
             <div>
-              <CardTitle>Total Income</CardTitle>
+              <CardTitle>{item.title}</CardTitle>
               <CardDescription className="text-xs">
-                This is the total income for the month.
+                This is the total {item.title} for the month.
               </CardDescription>
             </div>
             <Button variant="ghost">
@@ -26,15 +59,14 @@ export const Overview = () => {
             </Button>
           </CardHeader>
           <CardContent>
-            <p className="text-3xl font-bold">₱1,234</p>
+            <p className={cn("text-3xl font-bold", item.color)}>
+              {new Intl.NumberFormat("en-PH", {
+                style: "currency",
+                currency: "PHP",
+                maximumFractionDigits: 2,
+              }).format(item.amount)}
+            </p>
           </CardContent>
-          <CardFooter className="text-sm">
-            <div className="flex items-center">
-              <ArrowUp className="w-4 text-emerald-500" />
-              <span className="font-bold text-emerald-500">40%</span>
-              <p className="ml-1 text-slate-600">vs last month</p>
-            </div>
-          </CardFooter>
         </Card>
       ))}
     </div>
